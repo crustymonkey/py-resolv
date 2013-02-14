354ef45c00c99fc3c97672e2ee23973ae685be05
Jay Deiman <admin@splitstreams.com>
2013-02-13 16:22:17 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='py-resolv' ,
-    version='0.1.0' ,
+    version='0.2.0' ,
     author='Jay Deiman' ,
     author_email='admin@splitstreams.com' ,
     url='http://stuffivelearned.org' ,
